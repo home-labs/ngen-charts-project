606f13c77a73dc65097b7ae129c34022d6eea8b0
Rafael Pereira Laurindo <rplaurindo>
2018-09-21 17:08:23 -0300

    part of logic is done

--- projects/minimalist-ng-chart/src/lib/donut-clock-chart/component.ts
+++ projects/minimalist-ng-chart/src/lib/donut-clock-chart/component.ts
@@ -1,19 +1,73 @@
-import { Input } from "@angular/core";
+import {
+    Component,
+    Input
+} from '@angular/core';
 
 
+@Component({
+    selector: 'app-donut-clock-chart',
+    templateUrl: './template.html',
+    styleUrls: ['./style.sass']
+})
 export class DonutClockChartComponent {
 
     @Input()
-    ray: number;
+    ray: string;
+
+    @Input()
+    strokeWidth: string;
+
+    @Input()
+    diameter: string;
 
     @Input()
     ngClass: Object;
 
+    @Input()
+    percentualLength: number;
+
+    sectorLength: number;
+    xyNumericValue: number;
+
+    cx: string;
+    cy: string;
+
+    private rayNumericValue: number
     private circumferenceLength: number;
 
     construct() {
-        this.circumferenceLength = Math.PI * 2 * this.ray;
+        let
+            diameter: number;
+
+        this.rayNumericValue = parseFloat(this.ray);
+        diameter = 2 * this.rayNumericValue;
+        this.circumferenceLength = Math.PI * diameter;
+        this.sectorLength = this.circumferenceLength * (this.percentualLength/100);
+
+        this.resolveXY();
     }
 
+    private resolveXY() {
+
+        let
+            difference: number,
+            xyUnity: string = this.extractUnity(this.diameter),
+            strokeWidthNumericValue: number = parseFloat(this.strokeWidth);
+
+        this.xyNumericValue = parseFloat(this.diameter);
+
+        if (strokeWidthNumericValue > this.rayNumericValue) {
+            difference = strokeWidthNumericValue - this.rayNumericValue;
+            this.xyNumericValue -= difference;
+            this.diameter = `${this.xyNumericValue}${xyUnity}`
+        }
+
+        this.cx = `${(this.xyNumericValue/2) - (difference/2)}${xyUnity}`;
+        this.cy = this.cx;
+    }
+
+    private extractUnity(value: string = 'px'): string {
+        return '';
+    }
 
 }
--- projects/minimalist-ng-chart/src/lib/module.ts
+++ projects/minimalist-ng-chart/src/lib/module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 
+import { DonutClockChartComponent } from './donut-clock-chart/component';
+
 
 @NgModule({
     imports: [],
     declarations: [
-
+        DonutClockChartComponent
     ],
     exports: [
-
+        DonutClockChartComponent
     ]
 })
 export class MinimalistNgChartModule { }
