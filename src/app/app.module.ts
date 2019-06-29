c49458dadfe90a8af246714338631aa779eeebd3
Rafael Pereira Laurindo <rplaurindo>
2019-06-29 04:10:04 -0300

    adequations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
-import { MinimalistNgChartsModule } from 'minimalist-ng-charts';
-
 // import { MinimalistNgChartsModule } from 'projects/minimalist-ng-charts/src/public_api';
+import { MinimalistNgChartsModule } from 'minimalist-ng-charts';
 
 
 @NgModule({
