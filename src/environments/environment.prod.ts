e253d326f50b24e87f56702acf5124e93e9e7f26
Rafael Pereira <rafaelplaurindo@gmail.com>
2021-06-05 00:58:22 -0300

    adds comment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,6 @@
-// run $ ng build --watch
+// to deploy and test, install a HTTP server. Run $ npm i -g http-server;
+// do build, run $ ng build in the root path;
+// So start the project, inside dist/ngen-charts-project, run $ http-server.
 
 export const environment = {
     production: true
